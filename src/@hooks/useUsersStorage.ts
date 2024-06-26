2146a4257df6a47638c882cd15a520d7c8e5d161
michelwene <michelwene@hotmail.com>
2024-06-26 17:33:08 -0300

    feat: new-development hook para controlar estado dos usuários :sparkles:

---
+++ src/@hooks/useUsersStorage.ts
@@ -0,0 +1,56 @@
+import { useCallback } from 'react'
+
+import { useSessionStorage } from '@mantine/hooks'
+import superjson from 'superjson'
+
+type User = {
+  id: string
+  name: string
+  document: string
+  email: string
+  phone: string
+}
+
+type UsersStorage = User[]
+
+export const USERS_STORAGE = '@tinn-users-selection-v1.0.0'
+
+export const useUsersStorage = () => {
+  const [storedUsers, setStoredUsers] = useSessionStorage<UsersStorage>({
+    key: USERS_STORAGE,
+    getInitialValueInEffect: false,
+    serialize: superjson.stringify,
+    deserialize: (str) => (str === undefined ? [] : superjson.parse(str)),
+  })
+
+  const createUser = useCallback(
+    (user: User) => {
+      setStoredUsers((prev) => [...prev, user])
+    },
+    [setStoredUsers],
+  )
+
+  const removeUser = useCallback(
+    (id: string) => {
+      setStoredUsers((prev) => prev.filter((user) => user.id !== id))
+    },
+    [setStoredUsers],
+  )
+
+  const editUser = useCallback(
+    (user: User) => {
+      setStoredUsers((prev) => {
+        const index = prev.findIndex((prevUser) => prevUser.id === user.id)
+        if (index === -1) return prev
+
+        const newUsers = [...prev]
+        newUsers[index] = user
+
+        return newUsers
+      })
+    },
+    [setStoredUsers],
+  )
+
+  return { users: storedUsers, createUser, editUser, removeUser }
+}
