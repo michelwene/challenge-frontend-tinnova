d5d79c4d44dd5834d7c54053b8fda0c01a232d24
michelwene <michelwene@hotmail.com>
2024-07-03 12:06:24 -0300

    feat: development testes no componente button :sparkles:
      - Adiciona testes para verificar o shape do componente button.
      - Adiciona testes para verificar o texto do componente button.

--- src/components/@ui/button/button.spec.tsx
+++ src/components/@ui/button/button.spec.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import type { ComponentProps } from 'react'
 
 import { render } from 'src/@context'
 
@@ -6,9 +7,8 @@ import { Button } from '.'
 
 import '@testing-library/jest-dom'
 
-const renderComponent = () => {
-  return render(<Button>Test</Button>)
-
+const renderComponent = (props?: ComponentProps<typeof Button>) => {
+  return render(<Button {...props}>Test</Button>)
 }
 
 describe('Button component', () => {
@@ -16,5 +16,35 @@ describe('Button component', () => {
     renderComponent()
 
     expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+  it('should render button component with text', () => {
+    renderComponent()
+
+    expect(screen.getByText('Test')).toBeInTheDocument()
+  })
+
+  it('should render button component circle shape', () => {
+    renderComponent({
+      shape: 'circle',
+    })
+
+    expect(screen.getByRole('button')).toHaveStyle('border-radius: 50%')
+  })
+
+  it('should render button component default shape', () => {
+    renderComponent({
+      shape: 'default',
+    })
+
+    expect(screen.getByRole('button')).toHaveStyle('border-radius: 8px')
+  })
+
+  it('should render button component round shape', () => {
+    renderComponent({
+      shape: 'round',
+    })
+
+    expect(screen.getByRole('button')).toHaveStyle('border-radius: 32px')
   })
 })
